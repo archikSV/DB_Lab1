9a78f6b2ab9b481489838e670548bf69a458ef1a
SalchukArtem <archik.sv@gmail.com>
2024-01-08 21:49:38 +0200

    Task 1.8 completed. SELECT MIN().

--- lab1.1.sql
+++ lab1.1.sql
@@ -1,2 +1,3 @@
 -- Task 5: Specific quries
-SELECT DISTINCT group_name FROM Student_Grades;
+SELECT DISTINCT min_subject_name FROM Student_Grades
+WHERE average_grade_per_year = (SELECT MIN(average_grade_per_year) FROM Student_Grades)
